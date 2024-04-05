8b5d7225ce6b9693659cbc7b0c4850ba439f814b
eu <leitica@com>
2024-04-05 18:02:13 -0300

    botao trimestre

--- src/componentes/Projects.js
+++ src/componentes/Projects.js
@@ -10,32 +10,45 @@
  import fotoPost from "../assets/linguagensImg/conto.jpg";
  import fotoLivro from "../assets/linguagensImg/livro.jpg";
  import fotoAv1Mtm from "../assets/matematicaImg/Mapa.png";
+import React, { useState } from "react";
 
+  
 export const Projects = () => { /* Parte Natureza*/
+
+const [selectedCategory, setSelectedCategory] = useState("null");
+const [selectedTrimestre, setSelectedTrimestre] = useState("primeiro");
+
     const natureza = [
         {
             title: "Atividade Biologia",
             description: "Meme sobre evolucionismo.",
             imgUrl: fotoMeme,
-            link: "https://www.canva.com/design/DAF-u7idW64/JRRP6Lr7weS-WB_vzXKVFQ/edit?utm_content=DAF-u7idW64&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+            link: "https://www.canva.com/design/DAF-u7idW64/JRRP6Lr7weS-WB_vzXKVFQ/edit?utm_content=DAF-u7idW64&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+            trimestre: "primeiro"
         },
         {
             title: "Atividade 2",
             description: "Apresentação de uma revisão sobre química orgânica.",
             imgUrl: fotoOrganica,
-            link: "https://docs.google.com/presentation/d/14Vx3aqsnkBRJZKg75W40nZ45MMyyvhsWnyX9g_DmrEs/edit?usp=classroom_web&authuser=0"
+            link: "https://docs.google.com/presentation/d/14Vx3aqsnkBRJZKg75W40nZ45MMyyvhsWnyX9g_DmrEs/edit?usp=classroom_web&authuser=0",
+            trimestre: "primeiro"
+
         },
         {
             title: "Atividade 3",
             description: "Atividade prática de circuitos e resistores.",
             imgUrl: fotoCircuito,
-            link: "https://docs.google.com/document/d/1CV-GYZ9_vD-cs5GbQqaRbUw4TZKUeEzgbXZwvH4eo88/edit?usp=gmail"
+            link: "https://docs.google.com/document/d/1CV-GYZ9_vD-cs5GbQqaRbUw4TZKUeEzgbXZwvH4eo88/edit?usp=gmail",
+            trimestre: "primeiro"
+
         },
         {
             title: "Atividade 4",
             description: "Uma 'autoria' a partir da explicação \n durante a aula de Evolução e Teorias Evolucionistas.",
             imgUrl: fotoAutoria, 
-            link: "https://docs.google.com/document/d/1h5dyq6SbD9ZF4406HQ0Dt7itMWpMOrHRlOweCz0A2H0/edit?usp=drive_link"
+            link: "https://docs.google.com/document/d/1h5dyq6SbD9ZF4406HQ0Dt7itMWpMOrHRlOweCz0A2H0/edit?usp=drive_link",
+            trimestre: "primeiro"
+
         },
     ];
     const humanas = [
@@ -43,18 +56,23 @@ export const Projects = () => { /* Parte Natureza*/
             title: "Review Filme 1917",
             description: "Neste dia, assistimos o filme 1917 pois o \n mesmo retrata de uma época a qual estamos \n estudando e depois respondemos um questionário sobre o filme.",
             imgUrl: fotoFilme1917,
+            trimestre: "primeiro",
         },
         {
             title:  "Introdução à Geopolítica",
             description: "Escolhemos um país do qual deveríamos fazer uma apresentação \n falando sobre a situação atual do país.",
             imgUrl: fotoBandeiraMadagascar,
-            link: "https://docs.google.com/presentation/d/1dwZV5mdLD-fFOPZFDIsrQBRoYr9vpUWh8YdFACdEAhM/edit?usp=classroom_web&authuser=0"
+            link: "https://docs.google.com/presentation/d/1dwZV5mdLD-fFOPZFDIsrQBRoYr9vpUWh8YdFACdEAhM/edit?usp=classroom_web&authuser=0",
+            trimestre: "primeiro"
+
         },
         {
             title: "O Imperialismo na América",
             description: "Esta atividade requereu que nós fizessémos a análise de uma determinada imagem dada durante a aula para refletirmos em sala.  ",
             imgUrl: fotoPintura,
-            link: "https://docs.google.com/document/d/1HN822HfWsRRpgmdv9IiM2XrpMgPL78rtLNoKDbVBUj0/edit?usp=classroom_web&authuser=0"
+            link: "https://docs.google.com/document/d/1HN822HfWsRRpgmdv9IiM2XrpMgPL78rtLNoKDbVBUj0/edit?usp=classroom_web&authuser=0",
+            trimestre: "primeiro"
+
         },
        
     ];
@@ -63,13 +81,17 @@ export const Projects = () => { /* Parte Natureza*/
         {
             title:"Atividade Simple Past - Miniconto em Inglês",
             description:"Nessa atividade nós criamos um cartaz em formato de post do Instagram contando em forma de mini conto a história por trás da imagem que recebemos. ",
-            imgUrl: fotoPost
+            imgUrl: fotoPost,
+            trimestre: "primeiro"
+
         },
         {
             title: "Pré-Modernismo",
             description: "Escolhemos uma obra do período pré-modernista e fizemos uma apresentação sobre ela.",
             imgUrl: fotoLivro,
-            link: "https://www.canva.com/design/DAF-RUA3r9U/N0aYX2y9DmzXmZeRtIT3JA/edit?utm_content=DAF-RUA3r9U&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+            link: "https://www.canva.com/design/DAF-RUA3r9U/N0aYX2y9DmzXmZeRtIT3JA/edit?utm_content=DAF-RUA3r9U&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+            trimestre: "primeiro"
+
             
         }
     ]
@@ -78,7 +100,9 @@ export const Projects = () => { /* Parte Natureza*/
             title: "Arcos e Ângulos",
             description: "Produzimos um trilha dentro do ambiente da escola utilizando os conceitos de Arcos e Âgulos contando uma história por trás desta trilha. Competências e habilidades: C2 -Reconhecer e utilizar a linguagem algébrica e suas representações como a linguagem das ciências, necessária para expressar a relação entre grandezas e criar modelos descritivos permitindo conexões da matemática, com em fenômenos, sistemas naturais e tecnológicos.H 5- Ler e interpretar diferentes linguagens e representações envolvendo variações de grandezas; H 16- Compreender e construir modelos que correspondem a fenômenos periódicos.   ",
             imgUrl: fotoAv1Mtm,
-            link: "https://drive.google.com/file/d/12u5vl9DJyl-sXnJOfnOTBBAfvrZCsowM/view?usp=sharing"
+            link: "https://drive.google.com/file/d/12u5vl9DJyl-sXnJOfnOTBBAfvrZCsowM/view?usp=sharing",
+            trimestre: "primeiro"
+
         }
     ]
 
@@ -90,8 +114,20 @@ return(
             <Col>
           
           <h2 id="projetos">Projetos</h2>
+
+          <button className="trimestre" onClick={() => setSelectedTrimestre("primeiro")}> 
+            <span>1º Trimestre</span> &nbsp;  
+        </button>
+            <button className="trimestre" onClick={() => setSelectedTrimestre("segundo")}>
+            <span>2º Trimestre</span> &nbsp;  
+            </button>
+            <button className="trimestre" onClick={() => setSelectedTrimestre("terceiro")}>
+            <span>3º Trimestre</span>
+           </button>
           <p>Jazem aqui as principais atividades ocorridas ao longo do ano</p>
-            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+
+            <Tab.Container id="projects-tabs" activeKey={selectedCategory} onSelect={(key) => setSelectedCategory(key)}>
+
             <Nav variant="pills" className="nav-pills mb-5 justify-content-center alignt-items-center" id="pills-tab">
                 <Nav.Item>
                     <Nav.Link   eventKey="natureza">Natureza</Nav.Link>
@@ -110,55 +146,52 @@ return(
             <Tab.Content>
                 <Tab.Pane eventKey="natureza">
                    <Row>
-                    {
-                         natureza.map((natureza, index) => {
-                            return(
+                    
+                          {selectedCategory === "natureza" &&
+                          natureza.filter(natureza => natureza.trimestre === selectedTrimestre).map((natureza, index) => (
                                <Cards
                                 key={index}
                                 {...natureza} />
-                            )
-                        })
-                    }
+                            ))}
+                    
                    </Row>
                 </Tab.Pane>
                    <Tab.Pane eventKey="humanas">
                    <Row>
-                    {
-                         humanas.map((humanas, index) => {
-                            return(
+                    
+                        {selectedCategory === "humanas" &&
+                        humanas.filter(humanas => humanas.trimestre === selectedTrimestre).map((humanas, index) => (
                                <Cards
                                 key={index}
                                 {...humanas} />
-                            )
-                        })
-                    }
+                            
+                        ))}
+
                    </Row>
                    </Tab.Pane>
 
                    <Tab.Pane eventKey="linguagens">
                     <Row>
-                        {
-                            linguagens.map((linguagens, index) => {
-                                return(
+                        {selectedCategory === "linguagens" &&
+
+                        linguagens.filter(linguagens => linguagens.trimestre === selectedTrimestre).map((linguagens, index) => (
                                     <Cards 
                                     key={index}
                                     {...linguagens} />
-                                )
-                            })
-                        }
+                                ))}
+   
+                        
                     </Row>
                 </Tab.Pane>
                 <Tab.Pane eventKey="matematica">
                     <Row>
-                        {
-                            matematica.map((matematica, index) => {
-                                return(
-                                    <Cards 
+                        {selectedCategory === "matematica" &&
+                        matematica.filter(matematica => matematica.trimestre === selectedTrimestre).map((matematica, index) => (
+                                     <Cards
                                     key={index}
                                     {...matematica} />
-                                )
-                            })
-                        }
+                                ))}
+                
                     </Row>
                 </Tab.Pane>
             </Tab.Content>
--- src/componentes/ProjectsTech.js
+++ src/componentes/ProjectsTech.js
@@ -4,18 +4,26 @@
  import fotoCasoUML from "../assets/tecnicoImg/casoDeUso.png";
  import fotoPostman from "../assets/tecnicoImg/post.png";
  import fotoCRM from "../assets/tecnicoImg/implantacao.png"
+ import React, {useState} from "react";
 export const DevProjects = () => { /* Parte Desenvolvimento de Sistemas*/
+
+const [selectedCategory, setSelectedCategory] = useState("null");
+const [selectedTrimestre, setSelectedTrimestre] = useState("primeiro");
+
     const ads = [
         {
             title: "Calculadora de Horas",
             description: "Neste trabalho nós fizemos uma calculadora de horas em React.",
             imgUrl: fotoCalcReact,
             link: "https://github.com/leiticiasf/calculadoraReact",
+            trimestre: "primeiro"
         },
         {
             title: "Postman",
             description: "Primeiro contato com JAVA e Banco de Dados",
             imgUrl: fotoPostman,
+            trimestre: "primeiro"
+
         },
    
     ];      
@@ -26,6 +34,8 @@ export const DevProjects = () => { /* Parte Desenvolvimento de Sistemas*/
             description: "Esta atividade tinha como requerimento que nós criássemos um diagrama UML que explicasse o funcionamento do nosso site da SA. ",  
             imgUrl: fotoCasoUML,
             link: "https://lucid.app/lucidchart/d44af6f0-98d9-4c0b-b85c-38a961aa19c7/edit?viewport_loc=-201%2C399%2C1993%2C789%2C0_0&invitationId=inv_17bc7a94-ccf2-4de0-af71-5722ce18cc03",  
+            trimestre: "primeiro"
+
         }                                                       
     ]
 
@@ -33,7 +43,9 @@ export const DevProjects = () => { /* Parte Desenvolvimento de Sistemas*/
         {
             title: "CRM",
             description: "Nesta atividade nós criamos um cronograma como uma equipe de Implantação para então iniciar um Projeto de Implantação do sistema CRM, ainda em Desenvolvimento, para que todas as atividades necessárias para a implantação fossem coordenadas pela nossa  equipe, garantindo que a implantação por módulos seja realizada com sucesso",
-            imgUrl: fotoCRM
+            imgUrl: fotoCRM,
+            trimestre: "primeiro"
+
         }
     ]
 
@@ -45,8 +57,19 @@ return(
             <Col>
           
           <h2>Projetos Curso Técnico</h2>
-          <p>Jazem aqui as principais atividades do curso técnico ocorridas ao longo do ano</p>
-            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+
+          <button className="trimestre" onClick={() => setSelectedTrimestre("primeiro")}>
+            <span>1º Trimestre</span>
+            <div class="border full-rounded"></div></button>
+            <button className="trimestre" onClick={() => setSelectedTrimestre("segundo")}>
+            <span>2º Trimestre</span>
+            <div class="border full-rounded"></div></button>
+            <button className="trimestre" onClick={() => setSelectedTrimestre("terceiro")}>
+            <span>3º Trimestre</span>
+            <div class="border full-rounded"></div></button>
+          <p>Jazem aqui as principais atividades ocorridas ao longo do ano</p>
+
+            <Tab.Container id="projects-tabs" activeKey={selectedCategory} onSelect={(key) => setSelectedCategory(key)}>
             <Nav variant="pills" className="nav-pills mb-5 justify-content-center alignt-items-center" id="pills-tab">
                 <Nav.Item>
                     <Nav.Link eventKey="ads">Desen. de Sistemas</Nav.Link>
@@ -65,42 +88,38 @@ return(
             <Tab.Content>
                 <Tab.Pane eventKey="ads">
                    <Row>
-                    {
-                         ads.map((ads, index) => {
-                            return(
+                    {selectedCategory === "ads" &&
+                          ads.filter(ads => ads.trimestre === selectedTrimestre).map((ads, index) => (
                                <DevCards
                                 key={index}
                                 {...ads} />
-                            )
-                        })
-                    }
+                            ))}
+                    
                    </Row>
                 </Tab.Pane>
 
                 <Tab.Pane eventKey="modelagem">
                     <Row>
-                        {
-                            modelagem.map((modelagem, index) => {
-                                return(
+                        {selectedCategory === "modelagem" &&
+                           modelagem.filter(modelagem => modelagem.trimestre === selectedTrimestre).map((modelagem, index) => (
                                     <DevCards 
                                     key={index}
                                     {...modelagem} />
-                                )
-                            })
-                        }
+                                ))}
+                            
+                        
                     </Row>
                 </Tab.Pane>
 
                 <Tab.Pane eventKey="ims">
                     <Row>
-                        {
-                            ims.map((ims, index) => {
-                                return(
+                        {selectedCategory === "ims" &&
+                        ims.filter(ims => ims.trimestre === selectedTrimestre).map((ims, index) => (
                                     <DevCards 
                                     key={index}
                                     {...ims} />
                                 )
-                            })
+                            )
                         }
                     </Row>
                 </Tab.Pane>
